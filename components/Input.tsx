bf4415a6add7b0a5cbdac88e65da6c7d169af682
dexitional <ebenezerkb.ackah@gmail.com>
2022-12-21 19:27:42 +0000

    new

--- components/Input.tsx
+++ components/Input.tsx
@@ -20,7 +20,7 @@ function Input({ label,type,placeholder }: Props) {
           { false && type == 'password' && <Link href=""><span className="font-bold text-[0.95rem] text-yellow-800/90">Forgot Password?</span></Link> }
         </div>
         <div className="relative flex">
-          <input autoComplete="false" type={type == 'password' && show ? 'text':type} placeholder={placeholder} className="px-4 p-3.5 flex-1 rounded-md border border-gray-300 text-green-600 placeholder:text-gray-400 focus:caret-green-600 focus:border-none focus:outline-none focus:ring-2 focus:ring-green-600"/>
+          <input autoComplete="false" type={type == 'password' && show ? 'text':type} placeholder={placeholder} className="px-4 p-3.5 flex-1 rounded-md border border-gray-300 text-yellow-800/90 placeholder:text-gray-400 focus:caret-yellow-800/90 focus:border-none focus:outline-none focus:ring-2 focus:ring-yellow-800/90"/>
           { type == 'password' && <button onClick={showPwd} className="w-20 absolute top-1/4 right-1 z-20 font-bold text-sm text-yellow-800/90">{ show ? 'HIDE':'SHOW' }</button> }
         </div>
     </div>
