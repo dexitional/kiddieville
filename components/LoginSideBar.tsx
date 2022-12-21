6ac2d2628864678ec357c8cb94eddf3781011a63
dexitional <ebenezerkb.ackah@gmail.com>
2022-12-21 19:05:27 +0000

    new

--- components/LoginSideBar.tsx
+++ components/LoginSideBar.tsx
@@ -6,14 +6,14 @@ import Link from 'next/link'
 
 function LoginSideBar() {
   return (
-    <div className="relative w-[30%] h-full hidden md:flex flex-col justify-between bg-green-900 font-circular">
-        <img src={Bg.src} className="absolute z-2 w-full h-screen object-cover object-left opacity-30 blur-[5%] mix-blend-screen"/>
+    <div className="relative w-[30%] h-full hidden md:flex flex-col justify-between bg-blue-900 font-circular">
+        <img src={Bg.src} className="absolute z-2 w-full h-screen object-cover object-left opacity-20 blur-[5%] mix-blend-screen"/>
         <div className="p-8 z-10 flex flex-col text-white space-y-6 font-circular">
-          <img src={Logo.src} className="w-36 h-36 object-contain filter mix-blend-screen rounded opacity-70"/>
-          <h1 className="text-3xl font-bold">Welcome to <span className="text-yellow-400">KIDDIE VILLE</span><br/>Student Mangement System</h1>
+          <img src={Logo.src} className="w-36 h-36 object-contain filter mix-blend-screen rounded opacity-80"/>
+          <h1 className="text-3xl font-bold">Welcome to <span className="text-yellow-600/60">KIDDIE VILLE</span><br/>Student Mangement System</h1>
           <p className="font-circular">Apply for your Student Admission ( all modes. )</p>
         </div>
-        <div className="px-10 py-0 z-10 w-full h-[5.25rem] flex flex-row items-center justify-between bg-green-600 text-white">
+        <div className="px-10 py-0 z-10 w-full h-[5.25rem] flex flex-row items-center justify-between bg-yellow-700/60 text-white">
         <Link href="#">
             <span>Not a Student ? <span className="font-circular">Book an accomodation Now !</span></span>
         </Link>
