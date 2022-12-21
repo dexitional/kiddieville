a52af160c1615a03fcac5ad9172630585d1f5102
dexitional <ebenezerkb.ackah@gmail.com>
2022-12-21 19:35:34 +0000

    new

--- components/LoginContent.tsx
+++ components/LoginContent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../public/image.webp'
+import Logo from '../public/logo.jpg'
 import Button from './Button'
 import Input from './Input'
 import PhoneInput from './PhoneInput'
@@ -7,10 +7,11 @@ import Select from './Select'
 
 function LoginContent() {
   return (
-    <div className="px-10 md:px-20 md:py-16 h-full min-h-screen flex-1 flex flex-col space-y-10 bg-[#fbf9f9]">
+    <div className="px-10 md:px-20 py-5 md:py-16 h-full min-h-screen flex-1 flex flex-col space-y-10 bg-[#fbf9f9]">
       <div className="space-y-3">
-        <img src={Logo.src} className="w-20 h-20 flex md:hidden object-contain"/>
-        <h1 className="text-2xl font-bold">Login</h1>
+        <img src={Logo.src} className="w-20 h-20 rounded-lg flex md:hidden object-contain"/>
+        <h1 className="hidden sm:flex text-2xl font-bold">Login</h1>
+        <h1 className="sm:hidden text-2xl font-bold text-blue-900">KIDDIE VILLE MONTESSORI</h1>
         <p className="text-gray-500/90 font-medium font-circular">Provide your account information to continue</p>
       </div>
       <div>
--- components/LoginSideBar.tsx
+++ components/LoginSideBar.tsx
@@ -8,10 +8,10 @@ function LoginSideBar() {
   return (
     <div className="relative w-[30%] h-full hidden md:flex flex-col justify-between bg-blue-900 font-circular">
         <img src={Bg.src} className="absolute z-2 w-full h-screen object-cover object-left opacity-20 blur-[5%] mix-blend-screen"/>
-        <div className="p-8 z-10 flex flex-col text-white space-y-6 font-circular">
-          <img src={Logo.src} className="w-36 h-36 object-contain filter mix-blend-screen rounded opacity-80"/>
-          <h1 className="text-2xl font-bold"><span className="text-yellow-300/60 text-3xl"><span className="text-8xl text-yellow-500/60 italic">KIDDIE VILLE</span><br/> MONTESSORI</span><br/>Student Mangement System</h1>
-          <p className="font-circular">Apply for your Student Admission ( all modes. )</p>
+        <div className="p-8 z-10 flex flex-col items-center text-white space-y-6 font-circular">
+          <img src={Logo.src} className=" w-48 h-48 object-contain filter mix-blend-screen rounded-xl opacity-70"/>
+          <h1 className="text-2xl font-bold text-center"><span className="text-yellow-300/60 text-3xl"><span className="md:text-8xl text-yellow-500/60 italic">KIDDIE VILLE</span><br/> MONTESSORI</span><br/></h1>
+          <p className="font-circular hidden">Apply for your Student Admission ( all modes. )</p>
         </div>
         <div className="px-10 py-0 z-10 w-full h-[5.25rem] flex flex-row items-center justify-between bg-yellow-700/60 text-white">
         <Link href="#">
